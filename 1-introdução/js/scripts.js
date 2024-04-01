9da3a13cd8b5a5a2a1abdec7359b89282601199e
GUSTAVO PEREIRA DA MATA <gustavo.pmata@senacsp.edu.br>
2024-04-01 13:45:50 -0300

    01-04

---
+++ 1-introdução/js/scripts.js
@@ -0,0 +1,1 @@
+console.log("Agora estou nnum arquio externo")
---
+++ 3-estruturas/js/scripts.js
@@ -0,0 +1,86 @@
+// let nome = 'Gus'
+// console.log(nome)
+
+// nome = 'Gus Pereira'
+// console.log(nome)
+
+// const idade = 38
+// console.log(idade)
+
+// idade = 39
+// console.log(idade)
+
+// console.log(`O nome do usuario é: ${nome}
+// 'essa pessoa tem ${idade} anos`)
+
+// const name1 = prompt("Digite o seu nome")
+// console.log("o nome digitado foi" + name1)
+// console.log(`O nome digitado foi ${name1}`)
+// alert(`O nome digitado foi ${name1}`)
+
+// console.log("Teste")
+// console.error("Seu Jão")
+// console.warn("Alerta")
+
+// const m = 10
+
+// if(m > 5){
+//     console.log("M é maior que 5" + "Valor de M = " + m) 
+// }
+
+// const user = prompt("Digite seu nome")
+
+// if(user == 'Gus'){
+//     console.log(`bem vindo ${user}`)
+// }else{
+// console.log(`Uhm ... novo por aqui ${user}`)
+// }
+
+// const nota = prompt("Digite a média final")
+
+// if(nota >= 7){
+//     console.log("Aprovado")
+// }else{
+// console.log("Reprovado")
+// }
+
+// if(nota >= 7){
+//     console.log("Aprovado")
+// }else if(nota > 5){
+//     console.log("Recuperação forte")
+// }else if(nota > 6){
+//     console.log("Trabalho geral")
+// }else{
+//     console.log("Reprovado")
+// }
+
+// const diaSemana = parseInt(prompt("Dia da semana"))
+
+// if(diaSemana == 1){
+//     alert("Domingo")
+// }
+// if(diaSemana == 2){
+//     alert("Segunda")
+// }
+// if(diaSemana == 3){
+//     alert("Terça")
+// }
+// if(diaSemana == 4){
+//     alert("Quarta")
+// }
+// if(diaSemana == 5){
+//     alert("Quinta")
+// }
+// if(diaSemana == 6){
+//     alert("Sexta")
+// }
+// if(diaSemana == 7){
+//     alert("Sabado")
+// }else{
+//     alert("dia inválido")
+// }
+
+for(let i = 1; i<=10000; i++){
+    console.log(i);
+}
+
